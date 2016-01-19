629dfdf93c629a9cdc98fe3dc01776d9dff174e7
jmattefs <josephmattefs@gmail.com>
2016-01-18 18:02:03 -0600

    end of day one

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            head head = new head();
+            arm arm1 = new arm();
+            arm arm2 = new arm();
+            leg leg = new leg();
+        }
+    }
+}
---
+++ wreckingballarm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class wreckingballarm : arm
+    {
+        public bool hasChain = true;
+        public bool hasWreckingBall = true;
+    }
+}
